8d9db91e248e9ada8ec965fbddd72a8f961e42e2
aravalaharshavardhan <aravalaharsha@gmail.com>
2025-07-29 18:20:37 +0530

    Operate fn done

--- javascript.js
+++ javascript.js
@@ -8,3 +8,37 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+function subtract(a,b){
+    return a - b;
+}
+
+let firstnumber = '';
+let secondnumber = '';
+let curroperator = '';
+
+function operation(operator,a,b){
+    a = Number(a);
+    b = Number(b);
+
+    switch(operator){
+        
+        case "+" :
+            return add(a,b);
+
+        case "-":
+            return subtract(a,b);
+
+        case "/":
+             if(b == 0){
+                return "Invalid";
+             }
+             return divide(a,b);
+
+        case "*":
+            return multiply(a,b);
+
+        default:
+            return "Invalid operator";
+    }
+}
